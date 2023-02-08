cd95f16811285217e3092c1a39c3e75512c2aa6b
Michał Szczepanik <m.szczepanik@fz-juelich.de>
2023-02-08 14:24:54 +0100

    test: add test for export_records
    Similar to other commands, this checks whether the command succeeds
    and the written file is left in a clean state if the (mocked) api call
    returns some content.

---
+++ datalad_redcap/tests/test_export_form.py
@@ -0,0 +1,36 @@
+from unittest.mock import patch
+
+from datalad.api import export_redcap_form
+from datalad.distribution.dataset import Dataset
+from datalad_next.tests.utils import (
+    assert_status,
+    eq_,
+    with_credential,
+    with_tempfile,
+)
+
+TEST_TOKEN = "WTJ3G8XWO9G8V1BB4K8N81KNGRPFJOVL"  # needed to pass length assertion
+CSV_CONTENT = "foo,bar,baz\nspam,spam,spam"
+CREDNAME = "redcap"
+
+
+@with_tempfile
+@patch("datalad_redcap.export_form.Records.export_records", return_value=CSV_CONTENT)
+@with_credential(CREDNAME, type="token", secret=TEST_TOKEN)
+def test_export_writes_file(ds_path=None, mocker=None):
+    ds = Dataset(ds_path).create(result_renderer="disabled")
+    fname = "form.csv"
+
+    res = export_redcap_form(
+        url="https://www.example.com/api/",
+        forms=["foo"],
+        outfile=fname,
+        dataset=ds,
+        credential=CREDNAME,
+    )
+
+    # check that the command returned ok
+    assert_status("ok", res)
+
+    # check that the file was created and left in clean state
+    eq_(ds.status(fname, return_type="item-or-list").get("state"), "clean")

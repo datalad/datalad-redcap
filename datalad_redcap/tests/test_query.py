060861cda5289139a2cb76c48d581e3d00aae13b
Michał Szczepanik <m.szczepanik@fz-juelich.de>
2023-03-22 13:46:18 +0100

    refactor: clean up test_query
    An unused import is removed, and test code is blackened.

--- datalad_redcap/tests/test_query.py
+++ datalad_redcap/tests/test_query.py
@@ -3,14 +3,14 @@ from unittest.mock import patch
 from datalad.api import redcap_query
 from datalad_next.tests.utils import (
     assert_result_count,
-    with_credential,
 )
 
 JSON_CONTENT = {"foo": "bar"}
 
 
 def test_redcap_query_has_result(credman_filled, api_url):
-    with patch("datalad_redcap.query.MyInstruments.export_instruments", return_value=JSON_CONTENT):
-        assert_result_count(
-            redcap_query(url=api_url, result_renderer="disabled"), 1
-        )
+    with patch(
+        "datalad_redcap.query.MyInstruments.export_instruments",
+        return_value=JSON_CONTENT,
+    ):
+        assert_result_count(redcap_query(url=api_url, result_renderer="disabled"), 1)
